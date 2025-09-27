f2c9fb99d92613d4c78f8b311b49d4a4306d4cea
yangg205 <giangvvps41296@gmail.com>
2025-09-28 03:50:09 +0700

    thêm cam

--- camera_stream.py
+++ camera_stream.py
@@ -1,21 +1,23 @@
-from jetbot import Camera
 from flask import Flask, Response
 import cv2
 
-camera = Camera.instance(width=300, height=300)
-
 app = Flask(__name__)
+camera = cv2.VideoCapture(0)  # hoặc đọc frame ROS rồi convert sang OpenCV
 
-def gen_frames():
+def gen():
     while True:
-        frame = camera.value
-        _, buffer = cv2.imencode('.jpg', frame)
-        frame_bytes = buffer.tobytes()
+        ret, frame = camera.read()
+        if not ret:
+            continue
+        _, jpeg = cv2.imencode('.jpg', frame)
+        frame_bytes = jpeg.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
